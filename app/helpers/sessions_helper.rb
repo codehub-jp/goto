739cccbdc34856e2684fbc447b894739c7aa7275
codehub-jp <codehub3110@gmail.com>
2023-05-12 10:34:21 +0900

    fix

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
     def current_user
-        @test_current_user ||= TestUser.find_by(id: session[:test_user_id])
+        @current_user ||= TestUser.find_by(id: session[:test_user_id])
     end
 
     def logged_in?
-        !!test_current_user
+        !!current_user
     end
 end
