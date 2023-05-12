11ef10ba9e97068f666a7cc13a19b6885345d99a
codehub-jp <codehub3110@gmail.com>
2023-05-12 10:19:56 +0900

    save

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
-    def test_current_user
+    def current_user
         @test_current_user ||= TestUser.find_by(id: session[:test_user_id])
     end
 
-    def test_logged_in?
+    def logged_in?
         !!test_current_user
     end
 end
